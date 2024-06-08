646f03699460b5bcc03bc17d3fb295e0e9400956
rafiiAP <prafi726@gmail.com>
2024-06-08 14:26:06 +0700

    login, home, logout

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
-});
-
-Route::get('/login', function () {
     return view('pages.auth.auth-login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+       return view('pages.dashboard',['type_menu'=>'home']);
+    })->name('home');
 });
