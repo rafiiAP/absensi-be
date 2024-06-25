0c8fb908491e1bad29322d3d70964142e14b49f7
rafiiAP <prafi726@gmail.com>
2024-06-25 19:56:39 +0700

    view permission & detail permission view

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -33,5 +33,12 @@
                 </a>
             </li>
 
+            <li class="nav-item">
+                <a href="{{ route('permissions.index') }}" class="nav-link">
+                    <i class="fas fa-columns"></i>
+                    <span>Permission</span>
+                </a>
+            </li>
+
     </aside>
 </div>
