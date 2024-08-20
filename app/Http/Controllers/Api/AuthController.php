c95e47dec61dec2f9129c9d9cc90dc1959c69b84
rafiiAP <prafi726@gmail.com>
2024-08-21 03:29:11 +0700

    config firebase notif permisssion

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -23,12 +23,12 @@ class AuthController extends Controller
 
         //check user exist
         if (!$user) {
-            return response(['message' => 'Invalid credentials'], 401);
+            return response(['message' => 'Invalid user data not found'], 401);
         }
 
         //check password
         if (!Hash::check($loginData['password'], $user->password)) {
-            return response(['message' => 'Invalid credentials'], 401);
+            return response(['message' => 'Invalid Password'], 401);
         }
 
         $token = $user->createToken('auth_token')->plainTextToken;
@@ -48,23 +48,41 @@ class AuthController extends Controller
     public function updateProfile(Request $request)
     {
         $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            // 'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
             'face_embedding' => 'required',
         ]);
 
         $user = $request->user();
-        $image = $request->file('image');
+        // $image = $request->file('image');
         $face_embedding = $request->face_embedding;
 
         // //save image
-        $image->storeAs('public/images', $image->hashName());
-        $user->image_url = $image->hashName();
+        // $image->storeAs('public/images', $image->hashName());
+        // $user->image_url = $image->hashName();
         $user->face_embedding = $face_embedding;
         $user->save();
 
         return response([
             'message' => 'Profile updated',
             'user' => $user,
+        ], 200);
+    }
+
+    //update fcm_token
+    public function updateFcmToken(Request $request){
+
+        //ini  request
+        $request->validate([
+            'fcm_token' => 'required',
+        ]);
+
+        $user = $request->user();
+        $user->fcm_token = $request->fcm_token;
+        $user->save();
+
+        //ini response
+        return response([
+            'message' => 'Fcm token updated',
         ], 200);
     }
 
